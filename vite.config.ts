6a2c636623ed9aab44d62583fa023e8206a7cf16
Anastasia Chuvilova <achuvilova098@gmail.com>
2025-04-22 22:05:14 +0500

    Routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    publicDir: 'public',
     base: '/cinemaHall/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
+        assetsInlineLimit: 0
     }
 })
